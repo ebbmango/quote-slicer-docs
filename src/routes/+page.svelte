2634dba4ccf9f9e996f2d2804b846a15452cdf44
Emanuel Borges <ebbmango@tuta.io>
2025-11-29 14:13:20 +0100

    fixed broken link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="flex flex-col h-dvh w-full items-center justify-center pb-20">
     <h1 class="text-2xl">Quote Slicer Docs</h1>
     <p>Hello, world!</p>
-    <a href="/docs/overview">Overview</a>
+    <a href="/overview">Overview</a>
 </div>
