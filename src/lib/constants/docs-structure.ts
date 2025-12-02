3b3c2cdabfafa9e3fe5528da645cb07c6986b942
Emanuel Borges <ebbmango@tuta.io>
2025-12-02 18:52:52 +0100

    fixed paths

--- src/lib/constants/docs-structure.ts
+++ src/lib/constants/docs-structure.ts
@@ -1,28 +1,35 @@
+import { resolve } from '$app/paths';
+import type { Pathname } from '$app/types';
+import type { ResolvedPathname } from '$app/types';
+
 export type NavItem = {
 	title: string;
-	slug?: string;
+	slug?: ResolvedPathname;
 	children?: NavItem[];
 	icon?: any;
 };
 
 export const docsStructure: NavItem[] = [
-	{ title: 'Overview', slug: '/overview' },
+	{ title: 'Overview', slug: resolve('/overview') },
 	{
 		title: 'Components',
-		children: [{ title: 'Quote Workbench', slug: '/components/quote-workbench' }]
+		children: [{ title: 'Quote Workbench', slug: resolve('/components/quote-workbench') }]
 	},
 	{
 		title: 'Data Structures',
-		children: [{ title: 'Mapping', slug: '/data-structures/mapping' }]
+		children: [{ title: 'Mapping', slug: resolve('/data-structures/mapping') }]
 	},
 	{
 		title: 'Functionalities',
 		children: [
-			{ title: 'Tokenization', slug: '/functionalities/tokenization' },
-			{ title: 'Quote Tokenization', slug: '/functionalities/quote-tokenization' },
-			{ title: 'Translation Tokenization', slug: '/functionalities/translation-tokenization' }
+			{ title: 'Tokenization', slug: resolve('/functionalities/tokenization') },
+			{ title: 'Quote Tokenization', slug: resolve('/functionalities/quote-tokenization') },
+			{
+				title: 'Translation Tokenization',
+				slug: resolve('/functionalities/translation-tokenization')
+			}
 		]
 	},
 
-	{ title: 'Development Diary', slug: '/development-diary' }
+	{ title: 'Development Diary', slug: resolve('/development-diary') }
 ];
