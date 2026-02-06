ce05b9179561099e0d7efd48cd94a30d49af0c4a
Emanuel Borges <ebbmango@tuta.io>
2026-02-06 16:05:25 +0100

    visual: color rebalance

--- src/lib/constants/colors.ts
+++ src/lib/constants/colors.ts
@@ -1,11 +1,11 @@
 export const colors = [
-	{ name: 'Applesour', light: '#92A83B', dark: '#D9EA96' },
-	{ name: 'Lush', light: '#479D6F', dark: '#8AE1B2' },
-	{ name: 'Seabreeze', light: '#3EA0AA', dark: '#92D8E0' },
-	{ name: 'Azure', light: '#677BE0', dark: '#9EA8DC' },
-	{ name: 'Compostella', light: '#9C69DB', dark: '#C1A2E7' },
-	{ name: 'Sugar', light: '#D169D4', dark: '#DF9DE1' },
-	{ name: 'Strawberry', light: '#D1657B', dark: '#E396A6' },
-	{ name: 'Maple', light: '#D7844D', dark: '#ECB48F' },
-	{ name: 'Beeswax', light: '#BC9A15', dark: '#F3DF8D' }
+	{ name: 'Applesour', light: '#92A83B', dark: '#c0dc51' },
+	{ name: 'Lush', light: '#479D6F', dark: '#71e2a6' },
+	{ name: 'Seabreeze', light: '#3EA0AA', dark: '#77d7e2' },
+	{ name: 'Azure', light: '#677BE0', dark: '#8195f5' },
+	{ name: 'Compostella', light: '#9C69DB', dark: '#bc85ff' },
+	{ name: 'Sugar', light: '#D169D4', dark: '#e58ae8' },
+	{ name: 'Strawberry', light: '#D1657B', dark: '#f7879e' },
+	{ name: 'Maple', light: '#D7844D', dark: '#ffab73' },
+	{ name: 'Beeswax', light: '#BC9A15', dark: '#ffe681' }
 ];
