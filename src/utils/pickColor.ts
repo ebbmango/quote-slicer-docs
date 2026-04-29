98c3df7fae3e20fedda6cc41f69d4e1ee211377a
Emanuel Borges <ebbmango@tuta.io>
2026-04-29 09:26:44 +0200

    chore(styles): consolidate accent helpers

--- src/utils/pickColor.ts
+++
@@ -1,7 +0,0 @@
-import { colors } from '$lib/constants/colors';
-import type { Theme } from '$lib/types';
-
-export default function pickColor(index: number, theme: Theme): string {
-	const color = colors[index % colors.length];
-	return theme.dark ? color.dark : color.light;
-}
